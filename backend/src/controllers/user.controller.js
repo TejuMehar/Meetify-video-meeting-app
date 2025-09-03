d67c96ce18cebc86ad7e84c07a15babb99d86f13
Tejas mehar <tejas mehar7@gmail.com>
2025-09-03 12:08:33 +0530

    Write login and register route

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,33 +4,27 @@ import bcrypt, { hash } from "bcrypt"
 
 import crypto from "crypto"
 import { Meeting } from "../models/meeting.model.js";
+
+
 const login = async (req, res) => {
-
     const { username, password } = req.body;
-
     if (!username || !password) {
         return res.status(400).json({ message: "Please Provide" })
     }
-
     try {
         const user = await User.findOne({ username });
         if (!user) {
             return res.status(httpStatus.NOT_FOUND).json({ message: "User Not Found" })
         }
-
-
         let isPasswordCorrect = await bcrypt.compare(password, user.password)
-
         if (isPasswordCorrect) {
             let token = crypto.randomBytes(20).toString("hex");
-
             user.token = token;
             await user.save();
             return res.status(httpStatus.OK).json({ token: token })
         } else {
             return res.status(httpStatus.UNAUTHORIZED).json({ message: "Invalid Username or password" })
         }
-
     } catch (e) {
         return res.status(500).json({ message: `Something went wrong ${e}` })
     }
@@ -39,14 +33,11 @@ const login = async (req, res) => {
 
 const register = async (req, res) => {
     const { name, username, password } = req.body;
-
-
     try {
         const existingUser = await User.findOne({ username });
         if (existingUser) {
             return res.status(httpStatus.FOUND).json({ message: "User already exists" });
         }
-
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const newUser = new User({
@@ -54,15 +45,11 @@ const register = async (req, res) => {
             username: username,
             password: hashedPassword
         });
-
         await newUser.save();
-
         res.status(httpStatus.CREATED).json({ message: "User Registered" })
-
     } catch (e) {
         res.json({ message: `Something went wrong ${e}` })
     }
-
 }
 
 
