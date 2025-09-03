16433e7d459638eee5cebcda03aa329e108aea7f
Tejas mehar <tejas mehar7@gmail.com>
2025-09-03 12:25:02 +0530

    created user Schema in models folder

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,10 +2,22 @@ import mongoose, { Schema } from "mongoose";
 
 const userScheme = new Schema(
     {
-        name: { type: String, required: true },
-        username: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        token: { type: String }
+        name: {
+             type: String, 
+             required: true 
+            },
+        username: { 
+            type: String, 
+            required: true, 
+            unique: true 
+        },
+        password: { 
+            type: String, 
+            required: true 
+        },
+        token: { 
+            type: String 
+        }
     }
 )
 
