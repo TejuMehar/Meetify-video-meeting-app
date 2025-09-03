c6862a6f9fb3ba6bca5efd6a5334f7cec7c551fa
Tejas mehar <tejas mehar7@gmail.com>
2025-09-03 20:21:02 +0530

    Created Frontend SetUp

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import LandingPage from "./pages/landingPage"
+
+function App() {
+  
+
+  return (
+    <>
+      <LandingPage/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/landingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function landingPage() {
+  return (
+    <div>
+      <h1>landing Page </h1>
+    </div>
+  )
+}
