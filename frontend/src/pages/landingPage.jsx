ff9179d13f75a50bab727512add2fdc0737ba611
Tejas mehar <tejas mehar7@gmail.com>
2025-09-06 10:37:34 +0530

    Created Landing page

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import "../App.css"
+import { Link } from 'react-router-dom'
+
 
 export default function landingPage() {
   return (
-    <div>
-      <h1>landing Page </h1>
+    <div className='landingPageContainer'>
+        <nav>
+             <div className='navHeader'>
+               <h2>Meetify Video Call</h2>
+             </div>
+             <div className='navlist'>
+              <p>Join as Guest</p>
+              <p>Register</p>
+              <div role='button'>
+                <p>Login</p>
+              </div>
+             </div>
+        </nav>
+
+        <div className="landingMainContainer">
+            <div>
+              <h1> <span style={{color: "#FF9839"}}>Connect</span> With Your loved Ones</h1>
+
+              <p>Cover a Distance by Meetify Video Call </p>
+               <div role='button'>
+                  <Link to={"/auth"}>Get Started</Link>
+               </div>
+            </div>
+            <div>
+              <img src="/mobile.png" alt="" />
+            </div>
+        </div>
     </div>
   )
 }
